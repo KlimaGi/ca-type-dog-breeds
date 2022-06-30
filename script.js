55afc8a4e115051f3094260b2e5a203ea11d4e29
Giedre <giedre.klima@gmail.com>
2022-06-30 19:09:46 +0300

    select options generate

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let blockEl = document.querySelector("#block");
+let randomDogBreedFormEl = document.querySelector("#random-dog-breed-form");
+let dogBreedsSelectEl = document.querySelector("#dog-breeds-select");
+let imgEl = document.createElement("img");
+imgEl.src = "";
 
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((res) => res.json())
+  .then((breeds) => {
+    // console.log(breeds.message);
+    let breedsArr = Object.keys(breeds.message);
+    //console.log(breedsArr);
+    breedsArr.map((breed) => {
+      let breedOptionEl = document.createElement("option");
+      breedOptionEl.value = breed;
+      breedOptionEl.textContent = "-- " + breed;
+      dogBreedsSelectEl.append(breedOptionEl);
+
+      document.querySelector("#dog-breed-btn").removeAttribute("disabled");
+    });
+  });
+
+randomDogBreedFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let breedSelected = event.target.elements["dog-breeds-select"].value;
+  console.log(breedSelected);
+});
