2dea82655998f447e412f32233e865eb682d6393
Giedre <giedre.klima@gmail.com>
2022-07-01 11:00:27 +0300

    get images with sub-breeds

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let blockEl = document.querySelector("#block");
-let formBlockEl = document.querySelector("#form-block");
 let randomDogBreedFormEl = document.querySelector("#random-dog-breed-form");
 let dogBreedsSelectEl = document.querySelector("#dog-breeds-select");
 let imgEl = document.createElement("img");
@@ -7,32 +6,45 @@ imgEl.classList.add("img-block");
 
 fetch("https://dog.ceo/api/breeds/list/all")
   .then((res) => res.json())
-  .then((breeds) => {
-    console.log(breeds.message);
-    breedsObj = breeds.message;
-    let breedsKeys = Object.keys(breeds.message);
+  .then((data) => {
+    let breedsObj = data.message;
 
-    //console.log(breedsArr);
-    breedsKeys.map((breed) => {
-      let breedOptionEl = document.createElement("option");
-      breedOptionEl.value = breed;
-      breedOptionEl.textContent = "-- " + breed;
-      dogBreedsSelectEl.append(breedOptionEl);
+    for (key in breedsObj) {
+      let mainBreed = key;
+      let subBreeds = breedsObj[key];
 
-      document.querySelector("#dog-breed-btn").removeAttribute("disabled");
-    });
+      if (subBreeds.length === 0) {
+        let breedOptionEl = document.createElement("option");
+        breedOptionEl.textContent = "-- " + mainBreed;
+        breedOptionEl.value = mainBreed;
+        dogBreedsSelectEl.append(breedOptionEl);
+      } else {
+        subBreeds.map((subBreed) => {
+          let subBreedOptionEl = document.createElement("option");
+
+          subBreedOptionEl.textContent = `-- ${mainBreed} (${subBreed})`;
+          subBreedOptionEl.value = `${mainBreed}-${subBreed}`;
+          dogBreedsSelectEl.append(subBreedOptionEl);
+        });
+      }
+    }
+    document.querySelector("#dog-breed-btn").removeAttribute("disabled");
   });
 
 randomDogBreedFormEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  let breedSelected = event.target.elements["dog-breeds-select"].value;
-  console.log(breedSelected);
-  fetch(`https://dog.ceo/api/breed/${breedSelected}/images/random`)
+  let breedSelected = event.target.elements["dog-breeds-select"].value.replace(
+    "-",
+    "/"
+  );
+  // console.log(breedSelected.split("-").join("/"));
+
+  let url = `https://dog.ceo/api/breed/${breedSelected}/images/random`;
+
+  fetch(url)
     .then((res) => res.json())
     .then((images) => {
-      // console.log(images);
-      // console.log(images.message);
       imgEl.src = images.message;
-      formBlockEl.append(imgEl);
+      blockEl.append(imgEl);
     });
 });
